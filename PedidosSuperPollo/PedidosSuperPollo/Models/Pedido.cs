3df43c142394acfc40ec76676dc9845a68011b4e
Carlos Cuautle <191G0235@rcarbonifera.tecnm.mx>
2021-12-22 22:41:09 -0600

    Agregar archivos de proyecto.

---
+++ PedidosSuperPollo/PedidosSuperPollo/Models/Pedido.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PedidosSuperPollo.Models
+{
+    [Table("pedido")]
+    public class Pedido
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [NotNull]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        [NotNull]
+        public string Direccion { get; set; }
+
+        [NotNull]
+        public decimal Precio { get; set; }
+
+        [NotNull]
+        public string Hora { get; set; }
+
+        public string Cliente { get; set; }
+
+        [NotNull]
+        public int Estado { get; set; }
+
+    }
+}
