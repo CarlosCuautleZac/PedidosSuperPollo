e24058f9398406c5a1f220ebbbcc4f11790a0367
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-01-25 17:46:52 -0600

    Ajustes en las fuentes.

--- PedidosSuperPollo/PedidosSuperPollo/App.xaml.cs
+++ PedidosSuperPollo/PedidosSuperPollo/App.xaml.cs
@@ -3,6 +3,9 @@ using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
+[assembly: ExportFont("GothamBold.ttf", Alias = "GothamBold")]
+[assembly: ExportFont("Gotham-Black.otf", Alias = "GothamBlack")]
 namespace PedidosSuperPollo
 {
     public partial class App : Application
--- PedidosSuperPollo/PedidosSuperPollo/AssemblyInfo.cs
+++ PedidosSuperPollo/PedidosSuperPollo/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("GothamBold.ttf", Alias = "GothamBold")]
+[assembly: ExportFont("Gotham-Black.otf", Alias = "GothamBlack")]
