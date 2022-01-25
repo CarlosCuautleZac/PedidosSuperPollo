1decce113747fa32b999735362fcc22cad99008e
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-01-25 15:43:03 -0600

    Cambios en fuentes/Barra de estado

--- PedidosSuperPollo/PedidosSuperPollo.Android/MainActivity.cs
+++ PedidosSuperPollo/PedidosSuperPollo.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Views;
 
 namespace PedidosSuperPollo.Droid
 {
@@ -17,6 +18,14 @@ namespace PedidosSuperPollo.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+
+
+            Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+
+
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
