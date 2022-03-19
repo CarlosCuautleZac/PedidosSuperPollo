26d3e6fbddeda197d57943656da19f889929dc6e
191G0243@rcarbonifera.tecnm.mx <191G0243@rcarbonifera.tecnm.mx>
2022-03-18 22:53:15 -0600

    prueba sqlcommand

--- PedidosSuperPollo/PedidosSuperPollo/Models/PedidosContext.cs
+++ PedidosSuperPollo/PedidosSuperPollo/Models/PedidosContext.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace PedidosSuperPollo.Models
 {
     class PedidosContext
     {
+       SqlCommand ds
     }
 }
