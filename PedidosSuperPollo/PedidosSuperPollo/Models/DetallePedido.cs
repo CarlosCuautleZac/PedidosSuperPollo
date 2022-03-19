e9da31ca437362c0f5a42b1f84f94e90266c73c4
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2022-03-19 00:42:17 -0600

    cambios en models

--- PedidosSuperPollo/PedidosSuperPollo/Models/DetallePedido.cs
+++ PedidosSuperPollo/PedidosSuperPollo/Models/DetallePedido.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace PedidosSuperPollo.Models
 {
-    class DetallePedido
+    class DetallePedido:INotifyPropertyChanged
     {
         public int Id { get; set; }
         public int Cantidad { get; set; }
         public decimal MontoPagar { get; set; }
         public int IdPedido { get; set; }
         public int IdPlatillo { get; set; }
+
+        public void Actualizar(string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- PedidosSuperPollo/PedidosSuperPollo/Models/Pedido.cs
+++ PedidosSuperPollo/PedidosSuperPollo/Models/Pedido.cs
@@ -1,23 +1,65 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace PedidosSuperPollo.Models
 {
     
-    public class Pedido
+    public class Pedido:INotifyPropertyChanged
     {
-        
-        public int Id { get; set; }
-       
-        public DateTime Fecha { get; set; }
-        
-        public DateTime? HoraEntregado { get; set; }
-   
-        public DateTime HoraSolicitado { get; set; }
 
-        public string Direccion { get; set; }
-       
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; Actualizar(); }
+        }
+
+
+        private DateTime fecha;
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; Actualizar(); }
+        }
+
+
+        private DateTime? horaentregado;
+
+        public DateTime? HoraEntregado
+        {
+            get { return horaentregado; }
+            set { horaentregado = value; Actualizar(); }
+        }
+
+
+        private DateTime horasolicitado;
+
+        public DateTime HoraSolicitado
+        {
+            get { return horasolicitado; }
+            set { horasolicitado = value; Actualizar(); }
+        }
+
+
+        private string direccion;
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; Actualizar(); }
+        }
+
+
+        public void Actualizar(string nombre="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
