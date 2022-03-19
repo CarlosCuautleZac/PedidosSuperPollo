cb379a5980cac373728010a7da6cde828aaf8105
191G0243@rcarbonifera.tecnm.mx <191G0243@rcarbonifera.tecnm.mx>
2022-03-18 22:59:23 -0600

    context

--- PedidosSuperPollo/PedidosSuperPollo/Models/PedidosContext.cs
+++ PedidosSuperPollo/PedidosSuperPollo/Models/PedidosContext.cs
@@ -10,6 +10,29 @@ namespace PedidosSuperPollo.Models
 {
     class PedidosContext
     {
-       SqlCommand ds
+      SqlCommand comando;
+        SqlDataReader lector;
+        SqlConnection conexion = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SuperPollo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+ 
+         public ObservableCollection<Pedido> ListaPedidos { get; set; }
+
+        public ObservableCollection<Platillo> ListaPlatillos{ get; set; }
+
+
+           public void Conectar()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+           ~PedidosContext()
+        {
+            if(conexion.State!=ConnectionState.Closed)
+            {
+                conexion.Close();
+            }
+        }
     }
 }
