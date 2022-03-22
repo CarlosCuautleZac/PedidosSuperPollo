5813842b58d22c650712741a13954a865dfb9e68
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2022-03-21 20:34:42 -0600

    Codigo terminado de PedidosContext

---
+++ SQLQuery1.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[SpAgregarPedido]
+    @cant int,
+    @idplatillo int,
+    @direccion text
+
+AS
+    declare @idpedido int;
+    declare @time time;
+    declare @montopagar decimal;
+    declare @costounitario decimal;
+
+    SELECT @time = LTRIM(RIGHT(CONVERT(VARCHAR(20), GETDATE(), 100), 7));
+
+    insert into Pedido (Fecha, HoraSolicitado, Direccion) values (GETDATE(), @time, @direccion);
+
+    select @idpedido = max(id) from pedido;
+    select @costounitario = Precio_Unitario from Platillo where id=@idplatillo;
+
+    set @montopagar = @costounitario*@cant;
+
+    insert into DetallePedido(Cantidad, MontoPagar, IdPedido, IdPlatillo)
+    values (@cant, @montopagar, @idpedido,@idplatillo);
+
+
+RETURN 0
