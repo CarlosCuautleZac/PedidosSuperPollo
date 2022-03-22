a17ee0878361bab3a075283c2141ee098ac00163
CarlosCuautleZac <191g0235@rcarbonifera.tecnm.mx>
2022-03-21 20:16:51 -0600

    Seperacion de CRUD y Context

--- PedidosSuperPollo/PedidosSuperPollo/Models/PedidosContext.cs
+++ PedidosSuperPollo/PedidosSuperPollo/Models/PedidosContext.cs
@@ -12,11 +12,14 @@ namespace PedidosSuperPollo.Models
     {
         SqlCommand comando;
         SqlDataReader lector;
-        SqlConnection conexion = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SuperPollo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlConnection conexion = new SqlConnection
+            ("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SuperPollo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
         public ObservableCollection<Pedido> ListaPedidos { get; set; }
 
         public ObservableCollection<Platillo> ListaPlatillos { get; set; }
+
+        public ObservableCollection<DetallePedido> ListaDetallesPedido { get; set; }
 
         public void Conectar()
         {
@@ -37,6 +40,14 @@ namespace PedidosSuperPollo.Models
         public PedidosContext()
         {
             Conectar();
+            GetAllPedidos();
+        
+
+        }
+
+
+        public void GetAllPedidos()
+        {
             comando = new SqlCommand();
             comando.Connection = conexion;
             comando.CommandText = "select * from Pedidos;";
@@ -50,7 +61,7 @@ namespace PedidosSuperPollo.Models
                 {
                     Id = (int)lector["Id"],
                     Fecha = (DateTime)lector["Fecha"],
-                    HoraEntregado = (DateTime)lector["HoraEntregado"],
+                    HoraEntregado = Convert.IsDBNull(lector["HoraEntregado"]) ? null : (DateTime?)lector["HoraEntregado"],
                     HoraSolicitado = (DateTime)lector["HoraSolicitado"],
                     Direccion = (string)lector["Direccion"],
 
@@ -59,26 +70,47 @@ namespace PedidosSuperPollo.Models
                 ListaPedidos.Add(p);
             }
             lector.Close();
-
-
         }
 
 
-        public void Agregar(Pedido p)
+        public void GetAllDetallesPedido()
         {
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = "select * from DetallesPedido;";
+            lector = comando.ExecuteReader();
+            ListaDetallesPedido = new ObservableCollection<DetallePedido>();
 
-            var fecha = p.Fecha.ToString("dd-MM-yyyy");
+            while (lector.Read())
+            {
+                DetallePedido detallepedido = new DetallePedido()
+                {
+                    Id = (int)lector["Id"],
 
-                comando.CommandText = string.Format("insert into Pedido(Fecha," + "HoraEntregado,HoraSolicitado,Direccion) " +
-                    "values('{0}','{1}','{2}','{3}')", fecha, p.HoraEntregado, p.HoraSolicitado, p.Direccion);
-                comando.ExecuteNonQuery();
+                };
+            }
+        }
 
-                comando.CommandText = string.Format("select max(Id) from Pedido");
-                var id = comando.ExecuteScalar();
-                p.Id = (int)id;
-                ListaPedidos.Add(p);
+
+
+
+
+
+        //public void Agregar(Pedido p)
+        //{
+
+        //    var fecha = p.Fecha.ToString("dd-MM-yyyy");
+
+        //        comando.CommandText = string.Format("insert into Pedido(Fecha," + "HoraEntregado,HoraSolicitado,Direccion) " +
+        //            "values('{0}','{1}','{2}','{3}')", fecha, p.HoraEntregado, p.HoraSolicitado, p.Direccion);
+        //        comando.ExecuteNonQuery();
+
+        //        comando.CommandText = string.Format("select max(Id) from Pedido");
+        //        var id = comando.ExecuteScalar();
+        //        p.Id = (int)id;
+        //        ListaPedidos.Add(p);
 
             
-        }
+        //}
     }
 }
--- PedidosSuperPollo/PedidosSuperPollo/Repositories/PedidosRepository.cs
+++ PedidosSuperPollo/PedidosSuperPollo/Repositories/PedidosRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using PedidosSuperPollo.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace PedidosSuperPollo.Repositories
 {
     class PedidosRepository
     {
+        PedidosContext context = new PedidosContext();
+
+        public ObservableCollection<Pedido> ListaPedidos { get; set; }
+
+        public ObservableCollection<Platillo> ListaPlatillos { get; set; }
+
+        public ObservableCollection<DetallePedido> ListaDetallesPedido { get; set; }
+
+
+        public PedidosRepository()
+        {
+
+        }
+
+        //CRUD
+
+        //CREATE
+
+        //READ
+
+        
+
+
+        //UPDATE
+
+        //DELETE
+
+
     }
 }
